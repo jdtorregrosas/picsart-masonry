ee42d8a8f8b253e2da42717784f1662b4ef96113
Julian Torregrosa <julian.torregrosa@circulee.com>
2025-03-13 23:01:35 +0100

    chore: improve layout and add back button

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,25 @@
-import { Outlet } from "react-router";
-import { StyledFooter, StyledLayoutWrapper } from "./styled";
+import { Link, Outlet } from "react-router";
+import { HomeIcon } from "./home.icon";
+import {
+  StyledFooter,
+  StyledHeader,
+  StyledHeaderSubtitle,
+  StyledHeaderTitle,
+  StyledLayoutWrapper,
+} from "./styled";
 
 export const Layout = () => {
   return (
     <>
+      <StyledHeader>
+        <div>
+          <StyledHeaderTitle>Picsart Job application 2025</StyledHeaderTitle>
+          <StyledHeaderSubtitle>Julian Torregrosa</StyledHeaderSubtitle>
+        </div>
+        <Link to={"/"}>
+          <HomeIcon color="#c209c1" />
+        </Link>
+      </StyledHeader>
       <StyledLayoutWrapper>
         <Outlet />
       </StyledLayoutWrapper>
--- src/components/layout/styled.tsx
+++ src/components/layout/styled.tsx
@@ -2,15 +2,46 @@ import styled from "styled-components";
 
 export const StyledLayoutWrapper = styled.main`
   max-width: 1280px;
-  height: 100vh;
+  min-height: 100vh;
   margin: 0 auto;
+  padding: 32px 32px 64px 32px;
+  overflow: hidden;
+`;
+
+export const StyledHeader = styled.header`
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  min-height: 80px;
+  box-shadow: 0 1px 0 0 #e9e9e9;
+  background: #242424;
+  z-index: 10;
+  padding: 16px 32px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const StyledHeaderTitle = styled.h1`
+  color: #c209c1;
+  margin: 0;
+`;
+
+export const StyledHeaderSubtitle = styled.p`
+  color: #e9e9e9;
+  margin: 0;
 `;
 
 export const StyledFooter = styled.footer`
-  background: #f9f9f9;
+  background: #e9e9e9;
   position: fixed;
-  height: 72px;
+  height: 64px;
   bottom: 0;
   left: 0;
   right: 0;
+  color: #242424;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
